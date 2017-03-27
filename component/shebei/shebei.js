371c93c601cf8f57c3d42df593933de705819bd5
Wing Dou <WingDou@outlook.com>
2017-03-28 00:56:09 +0800

    ant表格弹框

--- component/shebei/shebei.js
+++ component/shebei/shebei.js
@@ -1,6 +1,7 @@
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu, Icon, Table, Button, Modal} from 'antd';
 import React, {Component} from 'react';
-import './shebei.css'
+import './shebei.css';
+const { Column, ColumnGroup } = Table;
 
 class Shebei extends React.Component {
     constructor(props){
@@ -9,11 +10,111 @@ class Shebei extends React.Component {
         this.state = {
 
         }
+        this.handleFun = this.handleFun.bind(this);
+    }
+
+    handleFun(record, event){
+        console.log(record, event);
+
+    }
+
+    info() {
+        Modal.info({
+            title: 'This is a notification message',
+            content: (
+            <div>
+                <p>some messages...some messages...</p>
+                <p>some messages...some messages...</p>
+            </div>
+            ),
+            onOk() {},
+        });
     }
     
     render() {
+        const columns = [
+            {
+                title: '设备名',
+                dataIndex: 'name',
+                key: 'name',
+            }, 
+            {
+                title: '公司',
+                dataIndex: 'age',
+                key: 'age',
+            }, 
+            {
+                title: '地址',
+                dataIndex: 'address',
+                key: 'address',
+            }, 
+            {
+                title: '详情',
+                key: 'action',
+                render: (text, record) => (
+                    <span>
+                    <Button onClick={info}>详情</Button>
+                    </span>
+                ),
+            }  
+         ];
+
+        const data = [{
+            key: '1',
+            firstName: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+            date:'2017-06-01',
+            offLine: true
+            }, {
+            key: '2',
+            firstName: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+            date:'2017-06-01',
+            offLine: true
+            }, {
+            key: '3',
+            firstName: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park',
+            date:'2017-06-01',
+            offLine: true
+        }];
+
         return (
-            <div className='shebei'>设备管理</div>
+            <Table dataSource={data}>
+                <Column
+                    title="First Name"
+                    dataIndex="firstName"
+                    key="firstName"
+                />
+                <Column
+                    title="Last Name"
+                    dataIndex="lastName"
+                    key="lastName"
+                />
+                <Column
+                    title="Age"
+                    dataIndex="age"
+                    key="age"
+                />
+                <Column
+                    title="Address"
+                    dataIndex="address"
+                    key="address"
+                />
+                <Column
+                    title="Action"
+                    key="action"
+                    render={(text, record) => (
+                        <span>
+                            <a href="#">Delete</a>
+                        </span>
+                    )}
+                    onCellClick ={this.handleFun}
+                />
+        </Table>
         );
     }
 }
