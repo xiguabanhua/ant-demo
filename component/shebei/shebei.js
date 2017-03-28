eba2ff46a28a8dabeb29f51958bbf406a2bb3b87
Wing Dou <WingDou@outlook.com>
2017-03-29 00:45:19 +0800

    弹框

--- component/shebei/shebei.js
+++ component/shebei/shebei.js
@@ -8,7 +8,8 @@ class Shebei extends React.Component {
         super(props);
         
         this.state = {
-
+            modal1Visible: false,
+            modal2Visible: false,
         }
         this.handleFun = this.handleFun.bind(this);
     }
@@ -17,18 +18,11 @@ class Shebei extends React.Component {
         console.log(record, event);
 
     }
-
-    info() {
-        Modal.info({
-            title: 'This is a notification message',
-            content: (
-            <div>
-                <p>some messages...some messages...</p>
-                <p>some messages...some messages...</p>
-            </div>
-            ),
-            onOk() {},
-        });
+    setModal1Visible(modal1Visible) {
+        this.setState({ modal1Visible });
+    }
+    setModal2Visible(modal2Visible) {
+        this.setState({ modal2Visible });
     }
     
     render() {
@@ -59,62 +53,84 @@ class Shebei extends React.Component {
             }  
          ];
 
-        const data = [{
-            key: '1',
-            firstName: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            date:'2017-06-01',
-            offLine: true
+        const data = [
+            {
+                key: '1',
+                firstName: 'John Brown',
+                age: 32,
+                address: 'New York No. 1 Lake Park',
+                date:'2017-06-01',
+                offLine: true
             }, {
-            key: '2',
-            firstName: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            date:'2017-06-01',
-            offLine: true
+                key: '2',
+                firstName: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park',
+                date:'2017-06-01',
+                offLine: true
             }, {
-            key: '3',
-            firstName: 'Joe Black',
-            age: 32,
-            address: 'Sidney No. 1 Lake Park',
-            date:'2017-06-01',
-            offLine: true
+                key: '3',
+                firstName: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park',
+                date:'2017-06-01',
+                offLine: true
         }];
-
+        let list = data.map((value, index, arr) => {
+            const objArray = Object.entries(value);
+            return (
+                <div>
+                    {objArray.map((value) => {
+                        return (
+                            <span>{value[0]}:{value[1]}</span>
+                        );
+                    })}
+                </div>
+            );
+        });
         return (
-            <Table dataSource={data}>
-                <Column
-                    title="First Name"
-                    dataIndex="firstName"
-                    key="firstName"
-                />
-                <Column
-                    title="Last Name"
-                    dataIndex="lastName"
-                    key="lastName"
-                />
-                <Column
-                    title="Age"
-                    dataIndex="age"
-                    key="age"
-                />
-                <Column
-                    title="Address"
-                    dataIndex="address"
-                    key="address"
-                />
-                <Column
-                    title="Action"
-                    key="action"
-                    render={(text, record) => (
-                        <span>
-                            <a href="#">Delete</a>
-                        </span>
-                    )}
-                    onCellClick ={this.handleFun}
-                />
-        </Table>
+            <div>
+                <Table dataSource={data}>
+                    <Column
+                        title="First Name"
+                        dataIndex="firstName"
+                        key="firstName"
+                    />
+                    <Column
+                        title="Last Name"
+                        dataIndex="lastName"
+                        key="lastName"
+                    />
+                    <Column
+                        title="Age"
+                        dataIndex="age"
+                        key="age"
+                    />
+                    <Column
+                        title="Address"
+                        dataIndex="address"
+                        key="address"
+                    />
+                    <Column
+                        title="Action"
+                        key="action"
+                        render={(text, record) => (
+                            <Button type="primary" onClick={() => this.setModal2Visible(true)}>Vertically centered modal dialog</Button>
+                        )}
+                        onCellClick ={this.handleFun}
+                    />
+            </Table>
+            <Modal
+                title="Vertically centered modal dialog"
+                wrapClassName="vertical-center-modal"
+                visible={this.state.modal2Visible}
+                onOk={() => this.setModal2Visible(false)}
+                onCancel={() => this.setModal2Visible(false)}
+            >
+                <p>some contents...</p>
+                <p>{list}</p>
+            </Modal>
+        </div>
         );
     }
 }
